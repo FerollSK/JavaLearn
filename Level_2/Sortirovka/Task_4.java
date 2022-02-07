317803cc4dd25dcbaabcd8be8b570309d86926e0
Sergey <Feroll.SK@gmail.com>
2022-02-07 21:55:44 +0300

    new

---
+++ Level_2/Sortirovka/Task_4.java
@@ -0,0 +1,24 @@
+package Level_2.Sortirovka;
+
+import java.util.Arrays;
+
+public class Task_4 {
+    public static void main(String[] args){
+    int[] massive = {1, 3, 4, 7, 9, 11, 14, 17};  
+    boolean isSorted = false;
+    int n;
+    while (!isSorted){
+        isSorted = true;
+        for (int i = 0; i < massive.length - 1; i++) {
+            if (massive[i] < massive[i + 1]){
+                isSorted = false;
+
+                n = massive[i];
+                massive[i] = massive[i + 1];
+                massive[i + 1] = n;
+            }
+        }
+    }
+    System.out.println(Arrays.toString(massive));
+    }
+}
---
+++ Level_2/Sortirovka/Task_8.java
@@ -0,0 +1,8 @@
+package Level_2.Sortirovka;
+
+public class Task_8 {
+    public static void main(String[] args){
+    double[] massive = {2/5, 1/6, 5/3, 9/4, 7/12, 6/10, 3/15};
+    
+    }
+}
