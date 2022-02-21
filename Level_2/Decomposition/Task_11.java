da24f136abfa8963e43238099c48730e573439e4
Sergey <Feroll.sk@gmail.com>
2022-02-21 21:15:33 +0300

    21.02

--- Level_2/Decomposition/Task_11.java
+++ Level_2/Decomposition/Task_11.java
@@ -2,7 +2,7 @@ package Level_2.Decomposition;
 
 public class Task_11 {
     public static void main(String[] args){
-        long a = 2384723470948109L;
+        long a = 2384723470948109345L;
         long b = 2389478927487482982L;
 
         twoNumbers(a, b);
@@ -13,8 +13,10 @@ public class Task_11 {
         int count2 = number(b);
         if (count1 > count2){
             System.out.println("В первом числе больше цифр");
-        } else{
+        } else if ( count1 < count2) {
             System.out.println("Во втором числе больше цифр");
+        } else {
+            System.out.println("В числах одинаковое количество цифр");
         }
     }
 
---
+++ Level_2/Decomposition/sdf.java
@@ -0,0 +1,11 @@
+package Level_2.Decomposition;
+
+import java.util.Random;
+
+public class sdf {
+    public static void main(String[] args){
+        Random rand = new Random();
+        int x = rand.nextInt();
+        System.out.println(x);
+    }
+}
